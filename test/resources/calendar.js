8cc334cd88e69bf744010af73e2512cae872bf05
Jenya Tkachenko <jekatka@outlook.com>
2018-09-14 20:46:27 +0200

    Clean up

--- test/resources/calendar.js
+++ test/resources/calendar.js
@@ -4,45 +4,32 @@ require('../testUtils');
 
 const expect = require('chai').expect;
 
+const alpaca = require('../..');
+
+alpaca.configure({
+  baseUrl: process.env.APCA_API_BASE_URL,
+  keyId: process.env.APCA_API_KEY_ID,
+  secretKey: process.env.APCA_API_SECRET_KEY
+});
+
 describe('calendar resource', function() {
   describe('get', function() {
     it('returns valid results without a parameter', function(done) {
-      const alpaca = require('../..');
-      alpaca.configure({
-        baseUrl: process.env.APCA_API_BASE_URL,
-        keyId: process.env.APCA_API_KEY_ID,
-        secretKey: process.env.APCA_API_SECRET_KEY
-      });
       expect(alpaca.getCalendar()).to.eventually.include('date').notify(done);
     });
 
     it('returns valid results with `start` parameter', function(done) {
       const alpaca = require('../..');
-      alpaca.configure({
-        baseUrl: process.env.APCA_API_BASE_URL,
-        keyId: process.env.APCA_API_KEY_ID,
-        secretKey: process.env.APCA_API_SECRET_KEY
-      });
       expect(alpaca.getCalendar('2018-01-01')).to.eventually.include('date').notify(done);
     });
 
     it('returns valid results with `end` parameter', function(done) {
       const alpaca = require('../..');
-      alpaca.configure({
-        baseUrl: process.env.APCA_API_BASE_URL,
-        keyId: process.env.APCA_API_KEY_ID,
-        secretKey: process.env.APCA_API_SECRET_KEY
-      });
       expect(alpaca.getCalendar(undefined, '2018-01-01')).to.eventually.include('date').notify(done);
     });
 
     it('returns valid results with both parameters', function(done) {
       const alpaca = require('../..');
-      alpaca.configure({
-        baseUrl: process.env.APCA_API_BASE_URL,
-        keyId: process.env.APCA_API_KEY_ID,
-        secretKey: process.env.APCA_API_SECRET_KEY
-      });
       expect(alpaca.getCalendar('2017-01-01', '2018-01-01')).to.eventually.include('date').notify(done);
     });
   });
--- test/resources/position.js
+++ test/resources/position.js
@@ -4,15 +4,17 @@ require('../testUtils');
 
 const expect = require('chai').expect;
 
+const alpaca = require('../..')
+
+alpaca.configure({
+  baseUrl: process.env.APCA_API_BASE_URL,
+  keyId: process.env.APCA_API_KEY_ID,
+  secretKey: process.env.APCA_API_SECRET_KEY,
+});
+
 describe('position resource', function() {
   describe('getAll', function() {
     it('returns valid results', function(done) {
-      const alpaca = require('../..')
-      alpaca.configure({
-        baseUrl: process.env.APCA_API_BASE_URL,
-        keyId: process.env.APCA_API_KEY_ID,
-        secretKey: process.env.APCA_API_SECRET_KEY,
-      });
       expect(alpaca.getPositions()).to.eventually.include('[').notify(done);
     });
   });
@@ -20,25 +22,11 @@ describe('position resource', function() {
   describe('getOne', function() {
     it('returns 422 error if unknown symbol is used', function(done) {
       const fakeSymbol = 'I don\'t exist!';
-      const alpaca = require('../..')
-      alpaca.configure({
-        baseUrl: process.env.APCA_API_BASE_URL,
-        keyId: process.env.APCA_API_KEY_ID,
-        secretKey: process.env.APCA_API_SECRET_KEY,
-      });
-
       expect(alpaca.getPosition(fakeSymbol)).to.be.rejectedWith('422').and.notify(done);
     });
 
     it('returns valid results if valid symbol is used; otherwise, 422', async function() {
       const symbol = '411101bc-4d72-4c07-acec-e9a2a8cbb9f5';
-      const alpaca = require('../..')
-      alpaca.configure({
-        baseUrl: process.env.APCA_API_BASE_URL,
-        keyId: process.env.APCA_API_KEY_ID,
-        secretKey: process.env.APCA_API_SECRET_KEY,
-      });
-
       try {
         const position = await alpaca.getPosition(symbol);
         expect(position).to.include('asset_id');
